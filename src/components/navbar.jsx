4987d1fc93b4274311c1819cab6755e7058360dc
John Baker <john@b4k3r.com>
2018-08-02 19:22:15 -0400

    -added bootstrap -created components folder -removed a couple files

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App';
+//import logo from 'Horizontal_Combination_Mark_RGB.png'
+
+const test1 = 'Horizontal_Combination_Mark_RGB.png'
+
+class NavBar extends Component {
+  render() {
+    return (
+      <img src={logo} alt="logo"/>
+    );
+  }
+}
+
+export default NavBar;
