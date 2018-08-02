fbbbe01f666dcaf8c4f04ec38821b85ffa64704d
dcruz87 <cruzdavidj@gmail.com>
2018-08-01 22:59:49 -0400

    idk

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-//import NavBar from './components/navbar.jsx'
-import test1 from './images/Icon_RGB.png';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <img src={test1} />
-        <button href="http://google.com/" className = "badge badge-primary m-3">wassup yall</button>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
