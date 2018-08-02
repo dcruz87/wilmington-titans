cde0c34aeea9659f18b446700136a53798588aff
John Baker <john@b4k3r.com>
2018-08-02 19:41:33 -0400

    back to functional throws no errors

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import logo from 'Horizontal_Combination_Mark_RGB.png'
+import App from '../App';
+import logotest from '../images/Horizontal_Combination_Mark_RGB.png';
 
 const test1 = 'Horizontal_Combination_Mark_RGB.png'
 
 class NavBar extends Component {
   render() {
     return (
-      <img src={logo} alt="logo"/>
+      <div>
+        <h1>another test</h1>
+        {/*<img src={logotest} alt="logo"/>*/}
+      </div>
+
     );
   }
 }
