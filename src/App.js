fc9df0393ebe3c3e10bd86a8bd82c04ac466d75c
John Baker <john@b4k3r.com>
2018-08-02 22:40:52 -0400

    added jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import NavBar from './components/navbar.jsx'
+import Jumbotron from './components/jumbotron.jsx'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>back to functional</h1>
+        {/*Load Navbar*/}
+        <NavBar/>
+        <Jumbotron/>
       </div>
     );
   }
---
+++ src/components/jumbotron.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+import './jumbotron.css'
+
+class Jumbotron extends Component {
+  render() {
+    return (
+      <div>
+        <div className = "jumbotron test">
+          <div className = "container-fluid">
+            <h1 className = "" >hi</h1>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default Jumbotron;
